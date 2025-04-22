8eaa2e037eeba895bbaf392788459ed25fe9344f
clement-queste <questeclement@gmail.com>
2025-04-22 17:18:47 +0200

    Ajout du projet PHP

--- CV.php
+++ CV.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+<?php require 'bdd1.php';?>
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -23,48 +24,45 @@
 
     <section class="create-section">
         <h2 class="fade-in" style="animation-delay: 0s;">Diplômes et Formation:</h2>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai obtenue mon Brevet des collèges Mention très bien en Juillet 2021 à Bernard Chochoy Norrent-Fontes.</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai obtenue mon Baccalauréat Général Mention Assez Bien en Juillet 2024 à Anatole France Lillers.</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je suis en cours d'optention du dîplome Deust WMI depuis septembre 2024 à l'ULCO de Calais.</p>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai obtenue mon Brevet des collèges Mention très bien en Juillet 2021 à Bernard Chochoy Norrent-Fontes.</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai obtenue mon Baccalauréat Général Mention Assez Bien en Juillet 2024 à Anatole France Lillers.</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je suis en cours d'optention du dîplome Deust WMI depuis septembre 2024 à l'ULCO de Calais.</li>
     </section>
 
     <section class="create-section">
         <h2 class="fade-in" style="animation-delay: 0s;">Expériences Professionnelles:</h2>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai fait un stage aux Cartonnages Vaillant en juin 2022 à Trezennes.</p>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai fait un stage aux Cartonnages Vaillant en juin 2022 à Trezennes.</li>
     </section>
 
     <section class="create-section">
         <h2 class="fade-in" style="animation-delay: 0s;">Compétences:</h2>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Premier Pro</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Photoshop</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Indesign</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Illustrator</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">After Effects</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Html</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">css</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">JavaScript</p>
+            <ul>
+                <?php foreach($listelogiciels as $logi){ ?>
+                <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;"><?= $logi['logi'] ?></li>
+                <?php } ?>
+            </ul>
     </section>
 
     <section class="create-section">
         <h2 class="fade-in" style="animation-delay: 0s;">Langues:</h2>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Anglais j'ai le niveau B2</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Espagnol j'ai le niveau B2</p>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Anglais j'ai le niveau B2</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Espagnol j'ai le niveau B2</li>
     </section>
 
     <section class="create-section">
         <h2 class="fade-in" style="animation-delay: 0s;">Qualités:</h2>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je sais Travailler en équipe</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je suis quelqu'un de très motivé</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je suis quelqu'un de très curieux</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai beaucoup de Créativité</p>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je sais Travailler en équipe</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je suis quelqu'un de très motivé</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">Je suis quelqu'un de très curieux</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai beaucoup de Créativité</li>
     </section>
 
     <section class="create-section">
         <h2 class="fade-in" style="animation-delay: 0s;">Loisirs:</h2>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai pratiqué le Basket-Ball pendant une année au club de Lillers.</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'adore les vêtements et la couture</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">La Music me passione de son écoute à la création</p>
-            <p class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai pratiqué le Football pendant 10 années au club de Norrent-Fontes.</p>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai pratiqué le Basket-Ball pendant une année au club de Lillers.</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'adore les vêtements et la couture</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">La Music me passione de son écoute à la création</li>
+            <li class="fade-in fade-in-delay" style="animation-delay: 0.5s;">J'ai pratiqué le Football pendant 10 années au club de Norrent-Fontes.</li>
     </section>
     </main>
 
