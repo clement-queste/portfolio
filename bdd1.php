445940f83bc396422b9b3c5a99163e034072545f
clement-queste <questeclement@gmail.com>
2025-04-24 13:35:07 +0200

    Ajout du projet PHP

--- bdd1.php
+++ bdd1.php
@@ -1,5 +1,7 @@
 <?php 
 
+//                     CV
+
 $listelogiciels = [
     ['id'=>'1','logi'=>'Premier Pro'],
     ['id'=>'2','logi'=>'Photoshop'],
@@ -11,17 +13,31 @@ $listelogiciels = [
     ['id'=>'8','logi'=>'JavaScript']
 ];
 
-$photoshop = [
-    ['id'=>'1','logi'=>'Premier Pro'],
-    ['id'=>'2','logi'=>'Photoshop'],
-    ['id'=>'3','logi'=>'Indesign'],
-    ['id'=>'4','logi'=>'Illustrator'],
-    ['id'=>'5','logi'=>'After Effects'],
-    ['id'=>'6','logi'=>'Html'],
-    ['id'=>'7','logi'=>'CSS'],
-    ['id'=>'8','logi'=>'JavaScript']
-];
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                Contact
 
 
 $nom = $_POST['nom'] ?? '';
