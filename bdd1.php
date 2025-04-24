78867e007f002c8d5faabb13476872f12c49f8f3
clement-queste <questeclement@gmail.com>
2025-04-24 12:59:49 +0200

    Ajout du projet PHP

--- bdd1.php
+++ bdd1.php
@@ -10,3 +10,14 @@ $listelogiciels = [
     ['id'=>'7','logi'=>'CSS'],
     ['id'=>'8','logi'=>'JavaScript']
 ];
+
+$photoshop = [
+    ['id'=>'1','logi'=>'Premier Pro'],
+    ['id'=>'2','logi'=>'Photoshop'],
+    ['id'=>'3','logi'=>'Indesign'],
+    ['id'=>'4','logi'=>'Illustrator'],
+    ['id'=>'5','logi'=>'After Effects'],
+    ['id'=>'6','logi'=>'Html'],
+    ['id'=>'7','logi'=>'CSS'],
+    ['id'=>'8','logi'=>'JavaScript']
+];
