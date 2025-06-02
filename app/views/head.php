24eacc2461c52fe18cbb58eb9f557700dd6a93a6
clement-queste <questeclement@gmail.com>
2025-06-02 16:02:00 +0200

    l

--- app/views/head.php
+++
@@ -1,8 +0,0 @@
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title><?= $page_title ?></title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-  <link rel="stylesheet" href="CSS/styles.css">
-  <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;600&display=swap" rel="stylesheet">
-</head>
